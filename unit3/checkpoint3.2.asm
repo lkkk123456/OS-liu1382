17b9c3d6cf9f7d417049a4521fba36e62c7ea2f7
jinnanliu <liu1382@flinders.edu.au>
2019-09-18 16:25:50 +0930

    fixing bug

--- unit3/checkpoint3.2.asm
+++ unit3/checkpoint3.2.asm
@@ -14,14 +14,9 @@
   .label current_screen_x = 7
   .label current_screen_line = 3
   .label current_screen_line_26 = 5
-  .label current_screen_line_42 = 5
+  .label current_screen_line_53 = 5
   .label current_screen_line_54 = 5
   .label current_screen_line_55 = 5
-  .label current_screen_line_56 = 5
-  .label current_screen_line_57 = 5
-  .label current_screen_line_61 = 5
-  .label current_screen_line_62 = 5
-  .label current_screen_line_63 = 5
 .segment Code
 main: {
     rts
@@ -343,7 +338,7 @@ reset: {
     jmp b1
 }
 test_memory: {
-    .const mem_end = $800
+    .label mem_end = $800
     .label p = $c
     .label value = 2
     .label mem_start = $a
@@ -354,79 +349,32 @@ test_memory: {
     sta.z p
     lda #>mem_end
     sta.z p+1
-    lda #0
-    sta.z current_screen_x
     lda #<$800
     sta.z mem_start
     lda #>$800
     sta.z mem_start+1
+    lda #0
+    sta.z current_screen_x
   b1:
-    lda.z mem_start+1
-    cmp #>$7fff
-    bcc b2
-    bne !+
-    lda.z mem_start
-    cmp #<$7fff
-    bcc b2
-  !:
-    lda.z current_screen_line
-    sta.z current_screen_line_57
-    lda.z current_screen_line+1
-    sta.z current_screen_line_57+1
-    lda #<message
-    sta.z print_to_screen.message
-    lda #>message
-    sta.z print_to_screen.message+1
-    jsr print_to_screen
-    lda.z mem_start
-    sta.z print_hex.value
-    lda.z mem_start+1
-    sta.z print_hex.value+1
-    lda.z current_screen_line
-    sta.z current_screen_line_61
-    lda.z current_screen_line+1
-    sta.z current_screen_line_61+1
-    jsr print_hex
-    lda.z current_screen_line
-    sta.z current_screen_line_56
-    lda.z current_screen_line+1
-    sta.z current_screen_line_56+1
-    lda #<message
-    sta.z print_to_screen.message
-    lda #>message
-    sta.z print_to_screen.message+1
-    jsr print_to_screen
-    lda.z current_screen_line
-    sta.z current_screen_line_62
-    lda.z current_screen_line+1
-    sta.z current_screen_line_62+1
-    lda #<mem_end
-    sta.z print_hex.value
-    lda #>mem_end
-    sta.z print_hex.value+1
-    jsr print_hex
-    jsr print_newline
-    rts
-  b2:
     lda #0
     sta.z value
-  b3:
+  b2:
     lda.z value
     cmp #$ff
-    bcc b4
-  b6:
+    bcc b3
+  b5:
     inc.z mem_start
     bne !+
     inc.z mem_start+1
   !:
     jmp b1
-  b4:
+  b3:
     lda.z value
     ldy #0
     sta (p),y
     lda (p),y
     cmp.z value
-    bne b6
+    bne b5
     lda.z current_screen_line
     sta.z current_screen_line_55
     lda.z current_screen_line+1
@@ -436,20 +384,12 @@ test_memory: {
     lda #>message
     sta.z print_to_screen.message+1
     jsr print_to_screen
-    lda.z current_screen_line
-    sta.z current_screen_line_63
-    lda.z current_screen_line+1
-    sta.z current_screen_line_63+1
-    lda #<mem_end
-    sta.z print_hex.value
-    lda #>mem_end
-    sta.z print_hex.value+1
     jsr print_hex
     jsr print_newline
     inc.z value
     lda #0
     sta.z current_screen_x
-    jmp b3
+    jmp b2
   .segment Data
     message: .text "the value is $"
     .byte 0
@@ -469,12 +409,20 @@ print_newline: {
 print_hex: {
     .label _4 = $e
     .label value = 8
+    lda #<test_memory.mem_end
+    sta.z value
+    lda #>test_memory.mem_end
+    sta.z value+1
     ldx #0
   b1:
     cpx #4
     bcc b2
     lda #0
     sta hex+4
+    lda.z current_screen_line
+    sta.z current_screen_line_53
+    lda.z current_screen_line+1
+    sta.z current_screen_line_53+1
     lda #<hex
     sta.z print_to_screen.message
     lda #>hex
